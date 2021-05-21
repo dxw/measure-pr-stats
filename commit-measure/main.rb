a95f0f3024e53d5a83a4f4bcb5e7b9697da44e09
F <f@dxw.com>
2021-05-21 15:19:39 +0200

    Drop ruby-git in favour of shelling out of Ruby

--- commit-measure/main.rb
+++ commit-measure/main.rb
@@ -1,27 +1,25 @@
-require 'git'
-require 'logger'
+# To execute:
+# bundle exec ruby commit-measure/main.rb dxw/rails-template 405a44fbfff17d014c0098065f3539800704f065
 
-ARGV
-
-# test command
-# bundle exec ruby commit-meaure/main.rb dxw/rails-template 405a44fbfff17d014c0098065f3539800704f065
+# To measure:
+# - code changes count
+#   - lines added
+#   - lines removed
+#   - files modified
+# - length of commit message
+# - file changes count
+#   - created
+#   - deleted
+#   - renamed
 
 repo_name = ARGV[0]
 commit_hash = ARGV[1]
 
+repo_path = "tmp/repos/#{repo_name}"
+
 `rm -r -f tmp`
 
-g = Git.clone("https://github.com/#{repo_name}", repo_name, path: "tmp", log: Logger.new(STDOUT))
-
-
-
-
-# length of commit message
-# files created/deleted/renamed
-
-# number of changes: lines add/lines removed, files modified
-
-# git show --format="" --shortstat 4ad4b8fc3dc34ae83b4af1e41746f5a844935966
-Dir.chdir("tmp/#{repo_name}")
+`git clone "https://github.com/#{repo_name}" #{repo_path}`
+Dir.chdir(repo_path)
 
 puts `git show --format="" --shortstat #{commit_hash}`
